658059037210bbcddad11d0ad4f5421f33b8ee8e
Zackmendel <111133398+Zackmendel@users.noreply.github.com>
2023-10-26 13:07:21 +0100

    Update Conclusion.py

--- Conclusion.py
+++ Conclusion.py
@@ -1,34 +1,41 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import numpy as np
-from millify import millify
 
 from st_on_hover_tabs import on_hover_tabs
 def test():
-  # Styling of metric container
-  st.markdown("""
-  <style>
-  div[data-testid="metric-container"] {
-     background-color: #424b43;
-     border: 3px solid #111212;
-     padding: 5% 5% 5% 10%;
-     border-radius: 10px;
-     color: white;
-     overflow-wrap: break-word;
-  }
-
-  /* breakline for metric text         */
-  div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
-     overflow-wrap: break-word;
-     white-space: break-spaces;
-     color: #B7e493;
-  }
-  </style>
-  """, unsafe_allow_html=True)
+  
 
   st.markdown("""
-  ### This section is meant to track Liquidity Provider's activities across different chains, while comparing Ethereum to it's Layer 2 blockchains.
+## **Conclusion:**
 
-  ### Charts in this section are :red[not] set to update daily.
+The Uniswap ecosystem has proven its mettle as a dominant force in the decentralized finance (DeFi) arena, with impressive trading volumes and a large and engaged user base. Over the past 30 days, Uniswap facilitated an astounding $19 billion in trades, with more than 8 million transactions conducted by over 1 million users. Ethereum continues to be the leading blockchain, commanding a significant margin in terms of trading activity on Uniswap.
+
+Uniswap's journey is nothing short of extraordinary, with nearly 197,000 liquidity pools created over time, currently locking approximately $7 billion in assets, and witnessing more than $1.3 trillion in liquidity provided and removed by liquidity providers (LPs).
+
+A significant trend emerges when assessing monthly liquidity flows, revealing that LPs consistently add more liquidity to Uniswap than they remove, contributing to the platform's overall stability. Notably, USDC-USDT, a stable-stable pool on Uniswap V3, stands as the largest pool on Ethereum, with over $475 million locked in its contract.
+
+When comparing pool types, it becomes evident that pools devoid of stables or ETH (referred to as "Other pools") exhibit the highest values in terms of inflow, outflow, and netflow. While Stable-ETH pools compete fiercely in terms of liquidity flows, their margin of balance left is on the higher side compared to Other pools.
+
+The growth in liquidity provision on Uniswap has been steadily on the rise over time, and nearly 1 million liquidity providers have engaged with the platform, conducting almost 4 million liquidity providing transactions. The balance between net minting and net liquidity provision remains positive over time, signifying that more LPs have provided liquidity than those who have removed it.
+
+Exploring the activity of liquidity providers in various pools reveals that some LPs have provided liquidity for extended periods without withdrawing. Additionally, a significant number of LPs have been active for up to a year on Uniswap in Ethereum, indicating sustained interest in DeFi liquidity provision.
+
+However, LP interest in Uniswap governance appears limited, as reflected in the small number of LPs holding the Uniswap native UNI token and actively participating in governance.
+
+## **Recommendations:**
+
+1. **Maintaining Ethereum Dominance**: Given Ethereum's stronghold in the Uniswap ecosystem, continue to enhance the Ethereum experience to attract and retain users.
+
+2. **Promoting Diversification**: Encourage LPs to diversify their liquidity across various pools to mitigate risks and contribute to a balanced ecosystem.
+
+3. **Enhancing Governance Participation**: To promote community involvement, consider incentivizing LPs to actively participate in governance decisions and discussions.
+
+4. **Supporting L2 Adoption**: As Layer 2 (L2) solutions show promise, consider further supporting their adoption, especially as the L2 ecosystem matures and provides scalability benefits.
+
+5. **Research and Analytics**: Invest in more detailed research to explore the LP crossover trend and identify opportunities to optimize user experiences.
+
+6. **User Education**: Provide educational resources for LPs to better understand the risks and rewards associated with liquidity provision.
+
+7. **Enhanced User Onboarding**: Streamline the onboarding process for new LPs to reduce barriers to entry and promote liquidity provision.
+
+Uniswap's journey is an inspiring one, and continued focus on liquidity provision and user engagement will be key to its enduring success in the DeFi landscape.
   """)
